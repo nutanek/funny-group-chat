93e388a5e3214280608998c43d4811e86f0e4f1d
nutanek <nutcs@hotmail.com>
2017-11-01 16:03:34 +0700

    create avatar

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class Avatar extends Component {
+    _generateColor(name) {
+        let size = name.length
+        const color = ['#f1c40f', '#e67e22', '#e74c3c', '#1abc9c', '#2ecc71', '#3498db', '#9b59b6']
+        return color[size%7]
+    }
+
+    _getFirstChar(name) {
+        let firstChar = name.charAt(0).toUpperCase()
+        return firstChar
+    }
+
+    render() {
+        let color = this._generateColor(this.props.name)
+        let firstChar = this._getFirstChar(this.props.name)
+        return (
+            <div className="avatar-wrapper" style={{backgroundColor: color}}>
+                {firstChar}
+            </div>
+        )
+    }
+}
--- src/components/UserOnline/index.js
+++ src/components/UserOnline/index.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react'
 import './index.css'
 import * as logger from './../../utils/logger'
+import Avatar from './../Avatar/'
 
 export default class UserOnline extends Component {
-    constructor() {
-        super()
-    }
-
     render() {
         return (
             <div className="col-xs-12 useronline-wrapper">
-                    
-                    {
-                        this.props.profiles.map((data, key) =>
-                            <div className="row" key={key}>
-                                <div className="col-xs-3">
-                                    <div className="avatar"></div>
-                                </div>
-                                <div className="col-xs-9">{data.info.name}</div>
+                {
+                    this.props.profiles.map((data, key) =>
+                        <div className="row" key={key}>
+                            <div className="col-xs-3">
+                                <Avatar name={data.info.name}/>
                             </div>
-                        )
-                    }
+                            <div className="col-xs-9">{data.info.name}</div>
+                        </div>
+                    )
+                }
             </div>
         )
     }
